37bd53a2b5eacdfab22ae9568f37035744eb62ad
Rishabh Pandey <rishabhpandey8092@gmail.com>
2022-09-15 16:40:37 +0530

    Udpaded sound button

--- JS/index.js
+++ JS/index.js
@@ -10,6 +10,7 @@ xhr.onload = function(){
 
 let btn = document.getElementById('button');
 let result = document.getElementById('result');
+let sound = document.getElementById('sounds');
 
 
 
@@ -27,17 +28,20 @@ fetchData = function(){
             // console.log(obj);
             result.innerHTML = `<div class="word">
             <h2>${input}</h2>
+            <i class="fa-sharp fa-solid fa-volume-high" onclick ="playSound()"></i>
             </div>
-        <div class="details">
+            <div class="details">
             <p>${data[0].phonetics[0].text || ""}</p>
             <p>${data[0].meanings[0].partOfSpeech}</p>
-        </div>
-        <div class="wordmeaning">
-        <P>${data[0].meanings[0].definitions[0].definition}</P>
-        </div><div class="example">
-        <P>Example : <br>${data[0].meanings[0].definitions[0].example || ""}</P>
-        </div>`;
-    }
+            </div>
+            <div class="wordmeaning">
+            <P>${data[0].meanings[0].definitions[0].definition}</P>
+            </div><div class="example">
+            <P>Example : <br>${data[0].meanings[0].definitions[0].example || ""}</P>
+            </div>`;
+            sound.setAttribute("src",`${data[0].phonetics[0].audio}`);
+            console.log(sound)
+        }
     else
     console.log("Kuch toh gadbad hai");
 }
@@ -45,5 +49,8 @@ fetchData = function(){
 xhr.send();
 
 }
-
 btn.addEventListener("click",fetchData);
+
+function playSound(){
+    sound.play();
+}
