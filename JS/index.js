6f74d06fbc6b173f609ee2eb4c4870599dc1ee75
Rishabh Pandey <rishabhpandey8092@gmail.com>
2022-09-15 14:22:30 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,49 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET',url,true);
+
+xhr.onload = function(){
+
+}
+
+
+let btn = document.getElementById('button');
+let result = document.getElementById('result');
+
+
+
+fetchData = function(){
+    let input = document.getElementById('searchWord').value;
+    // console.log(input);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET',`${url}${input}`,true);
+    
+    
+    xhr.onload = function(){
+        if(this.status === 200){
+            let data = JSON.parse(this.responseText);
+            // console.log(obj);
+            result.innerHTML = `<div class="word">
+            <h2>${input}</h2>
+            </div>
+        <div class="details">
+            <p>${data[0].phonetics[0].text || ""}</p>
+            <p>${data[0].meanings[0].partOfSpeech}</p>
+        </div>
+        <div class="wordmeaning">
+        <P>${data[0].meanings[0].definitions[0].definition}</P>
+        </div><div class="example">
+        <P>Example : <br>${data[0].meanings[0].definitions[0].example || ""}</P>
+        </div>`;
+    }
+    else
+    console.log("Kuch toh gadbad hai");
+}
+
+xhr.send();
+
+}
+
+btn.addEventListener("click",fetchData);
